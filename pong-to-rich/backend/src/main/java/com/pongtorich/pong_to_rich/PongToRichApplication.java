d50a355ca85a74f4076353074fff6411de7b003a
Pong <parkdanil@naver.com>
2026-04-07 18:15:04 +0900

    init: pong-to-rich backend 프로젝트 초기 세팅 및 docker-compose에 docker-network 기반 MySQL 환경 구성

---
+++ pong-to-rich/backend/src/main/java/com/pongtorich/pong_to_rich/PongToRichApplication.java
@@ -0,0 +1,13 @@
+package com.pongtorich.pong_to_rich;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PongToRichApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PongToRichApplication.class, args);
+	}
+
+}
---
+++ pong-to-rich/backend/src/test/java/com/pongtorich/pong_to_rich/PongToRichApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pongtorich.pong_to_rich;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PongToRichApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
