5c938c5c514c2aab8904c343d1b48411cdf587bf
Pong <parkdanil@naver.com>
2026-04-09 18:46:39 +0900

    feat: Swagger JWT 인증 설정, API 문서 설명/예시값 추가

--- pong-to-rich/backend/src/main/java/com/pongtorich/pong_to_rich/config/SwaggerConfig.java
+++ pong-to-rich/backend/src/main/java/com/pongtorich/pong_to_rich/config/SwaggerConfig.java
@@ -1,7 +1,10 @@
 package com.pongtorich.pong_to_rich.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,6 +17,13 @@ public class SwaggerConfig {
                 .info(new Info()
                         .title("Pong-to-Rich API")
                         .description("주식 자동매매 플랫폼 API 문서")
-                        .version("v0.0.1"));
+                        .version("v0.0.1"))
+                .addSecurityItem(new SecurityRequirement().addList("Bearer"))
+                .components(new Components()
+                        .addSecuritySchemes("Bearer", new SecurityScheme()
+                                .name("Bearer")
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")));
     }
 }
